29c049f20254d80ce11a33bb6523587a23eea40f
Ariel Hernan Cubito <acubito@afascl.coop>
2023-07-25 10:05:37 -0300

    Initial commit

---
+++ src/Variables.java
@@ -0,0 +1,30 @@
+public class Variables {
+    public static void main (String[] args) {
+
+        //Variables primitivos y referencias
+
+        /*
+            Definicion de variables camelCase
+
+            "variableEntera" "variableEnteraCompuesta"
+
+            los caracteres solo unicode
+
+         */
+
+        byte varByte;
+        short varShort;
+        int varInt;
+        long varLong;
+        boolean varBolean;
+        double varDouble;
+        float varFloat;
+
+        char varChar;
+
+        String varString = "Curso Java";
+
+        System.out.println(varString.toLowerCase());
+
+    }
+}
